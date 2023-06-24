ab72efa172a5324c079a6d1a9c641316a89a48a9
Никита Пискунов <nikpna@yandex.ru>
2023-06-24 15:36:03 +0700

    try

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.161.0.160', '127.0.0.1', 'localhost', 'smilerightnow.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
